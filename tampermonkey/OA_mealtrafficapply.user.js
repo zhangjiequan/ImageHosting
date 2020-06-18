75564ab965f1f33f4fa17bf8367269f4eb31ba9a
zhangjiequan <31504745+zhangjiequan@users.noreply.github.com>
2020-06-18 17:22:49 +0800

    Update OA_mealtrafficapply.user.js

--- tampermonkey/OA_mealtrafficapply.user.js
+++ tampermonkey/OA_mealtrafficapply.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OA_工作日餐费交通费申请_固定选人
 // @namespace    https://github.com/zhangjiequan/
-// @version      0.3
+// @version      0.4
 // @updateURL    https://github.com/zhangjiequan/ImageHosting/raw/master/tampermonkey/OA_mealtrafficapply.user.js
 // @downloadURL  https://github.com/zhangjiequan/ImageHosting/raw/master/tampermonkey/OA_mealtrafficapply.user.js
 // @description  OA_工作日餐费交通费申请_固定选人（不用插件，默认的话，总是选了第一个，不科学）
@@ -12,14 +12,66 @@
 
 //OA_mealtrafficapply.js
 
-(function() {
+(function () {
     // Your code here...
     $(function () {
-        var leader = document.getElementById("leader3628")
-        if (leader) {
-            leader.checked = true;
-            console.log("OA_mealtrafficapply version      0.3");
+        var kLocalStorageLeaderKey = "OA_mealtrafficapply_localStorage_item_name_selectedLeader"
+        var enableLog = false
+
+        var log = function(msg) {
+            if (!enableLog){
+                return
+            }
+            console.log(msg)
         }
+
+        var isSuportStorage = function () {
+            return typeof (Storage) != "undefined"
+        }
+
+        var getSelectedLeader = function () {
+            var nextStepRadios = document.getElementsByName("nextStepRadio")
+            for (var i = 0; i < nextStepRadios.length; i++) {
+                if (nextStepRadios[i].checked) {
+                    return nextStepRadios[i].id
+                };
+            }
+        }
+
+        var saveSelectedLeaderId = function () {
+            if (!isSuportStorage()) {
+                return
+            }
+            var value = getSelectedLeader()
+            localStorage.setItem(kLocalStorageLeaderKey, value);
+            log("saveSelectedLeaderId" + value);
+        }
+
+        var getSavedSelectedLeaderId = function () {
+            if (!isSuportStorage()) {
+                return ""
+            }
+            var value = localStorage.getItem(kLocalStorageLeaderKey);
+            log("getSavedSelectedLeaderId" + value);
+            return value
+        }
+
+        var autoSelectLeader = function () {
+            var leaderId = getSavedSelectedLeaderId()
+            var leader = document.getElementById(leaderId)
+            if (leader) {
+                leader.checked = true;
+                log("OA_mealtrafficapply leaderauto selected" + leaderId);
+            }
+        }
+
+        var calledInterval = function () {
+            log("calledInterval");
+            saveSelectedLeaderId()
+        }
+
+        autoSelectLeader()
+        setInterval(function () { calledInterval() }, 1000);
     });
 
 })();
