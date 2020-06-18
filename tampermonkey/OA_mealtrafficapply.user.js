a85da222110c823aa4833dad80827b584165b3c6
zhangjiequan <31504745+zhangjiequan@users.noreply.github.com>
2020-06-18 16:34:28 +0800

    Update OA_mealtrafficapply.user.js

--- tampermonkey/OA_mealtrafficapply.user.js
+++ tampermonkey/OA_mealtrafficapply.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         OA_工作日餐费交通费申请_固定选人
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/zhangjiequan/
 // @version      0.1
-// @updateURL    http://10.18.19.100/plugins/tampermonkey/OA_mealtrafficapply.user.js
-// @downloadURL  http://10.18.19.100/plugins/tampermonkey/OA_mealtrafficapply.user.js
-// @description  try to take over the world!
+// @updateURL    https://github.com/zhangjiequan/ImageHosting/raw/master/tampermonkey/OA_mealtrafficapply.user.js
+// @downloadURL  https://github.com/zhangjiequan/ImageHosting/raw/master/tampermonkey/OA_mealtrafficapply.user.js
+// @description  OA_工作日餐费交通费申请_固定选人（不用插件，默认的话，总是选了第一个，不科学）
 // @author       zhangjiequan
 // @match        http://oa.info/mealtrafficapply/draft.do*
 // @grant        none
